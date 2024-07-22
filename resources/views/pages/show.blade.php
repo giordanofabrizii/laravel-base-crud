c5f4d741c6b5a18ec26e06c34a4bf7520fd8a90e
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-22 14:51:28 +0200

    added scss and info

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -8,11 +8,16 @@
 
     <section class="show-animal">
         <article>
-            <h1>{{ $animal->nome } <span>- {{ $animal->specie }}</span></h1>
-            <div class="info">
-                <p>{{ $animal->razza }}</p>
-            </div>
             <img src="{{ $animal->url_img }}" alt="{{ $animal->nome }} img">
+            <div class="text">
+                <h1>{{ $animal->nome }} <span>- {{ $animal->specie }}</span></h1>
+                <h2>{{ $animal->razza }}</h2>
+                <div class="info">
+                    <p><i class="fa-solid fa-weight-hanging"></i> {{ $animal->peso }} kg</p>
+                    <p><i class="fa-solid fa-ruler"></i> {{ $animal->altezza }} cm</p>
+                </div>
+                <h4>{{ $animal->info }}</h4>
+            </div>
         </article>
     </section>
 
